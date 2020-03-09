d2c7294f0842ff618a41b59e45929f269dc53d28
SBR001168 <SBR001168@SPMWS94561J1.saipemnet.saipem.intranet>
2020-03-09 20:04:29 -0300

    mascara + IDENTITY

--- ControlePatrimonio/src/main/java/edu/infnet/patrimonio/negocio/modelo/Patrimonio.java
+++ ControlePatrimonio/src/main/java/edu/infnet/patrimonio/negocio/modelo/Patrimonio.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 public class Patrimonio {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Integer id;
 	@Column
 	private String descricao;
---
+++ ControlePatrimonio/src/main/webapp/js/patrimonio.js
@@ -0,0 +1,5 @@
+var montarMascaras = function(){
+
+	$('#id').mask("000000");		
+
+};
