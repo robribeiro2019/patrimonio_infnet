5a0cd7ac937f20bdcf6f3386e4a71691f1b1ffa2
Lucas Alonso Silva <alonsosilva.lucas@gmail.com>
2020-03-10 00:59:14 -0300

    trabalho concluido

--- ControlePatrimonio/src/main/java/edu/infnet/patrimonio/controler/PatrimonioServlet.java
+++ ControlePatrimonio/src/main/java/edu/infnet/patrimonio/controler/PatrimonioServlet.java
@@ -33,25 +33,7 @@ public class PatrimonioServlet extends HttpServlet {
 		
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		String action = (String) req.getParameter("action");
-		String id = (String) req.getParameter("id");
-		montarPatrimomio(req);
-		
-		if("new".equals(action)) {
-			patrimonioService.salvarPatrimonio(patrimonio);
-		}
-		
-		if("edit".equals(action)) {
-			patrimonio.setId(Integer.parseInt(id));
-			patrimonioService.atualizarPatrimonio(patrimonio);			
-		}
-		
-		/* Montar Lista de Produtos */				
-		listaPatrimonios =  patrimonioService.listaPatrimonios();
-		req.setAttribute("listaPatrimonios", listaPatrimonios);		
-		req.getRequestDispatcher("pages/home.jsp").forward(req, resp);
-				
+		doGet(req, resp);
 	}
 	
 	@Override
@@ -59,37 +41,34 @@ public class PatrimonioServlet extends HttpServlet {
 		
 		String action = (String) req.getParameter("action");
 		String id = (String) req.getParameter("id");
-		
+		String descricao = req.getParameter("descricao");
+		String localizacao = req.getParameter("localizacao");
 		String recurso = "pages/home.jsp";
 		
 		if("new".equals(action)) {
-			recurso = "pages/patrimonio.jsp";
+			if(id != null) {
+				patrimonioService.atualizarPatrimonio(new Patrimonio(descricao, localizacao));
+			}
+			recurso = "pages/home.jsp";
 		}
 		
-		if("edit-form".equals(action)) {
+		if("edit".equals(action)) {
 			patrimonio = patrimonioService.buscarPatrimonio(Integer.parseInt(id));
-			req.setAttribute("patrimonio", patrimonio);
+			req.setAttribute("patrimonio", new Patrimonio(Integer.parseInt(id), descricao, localizacao));
 			recurso = "pages/patrimonio.jsp";
 		}
 						
 		if("delete".equals(action)) {
-			
 			patrimonio = patrimonioService.buscarPatrimonio(Integer.parseInt(id));
 			
 			if(!Objects.isNull(patrimonio)) {
 				patrimonioService.removerPatrimonio(patrimonio);
 			}			
-		}			
-		
+		}
 		listaPatrimonios =  patrimonioService.listaPatrimonios();
 		req.setAttribute("listaPatrimonios", listaPatrimonios);
-		req.getRequestDispatcher(recurso).forward(req, resp);		
+		req.getRequestDispatcher(recurso).forward(req, resp);
+		
 	}
 	
-	public void montarPatrimomio(HttpServletRequest req) {
-		patrimonio = new Patrimonio();
-		patrimonio.setId(Integer.parseInt(req.getParameter("Id")));
-		patrimonio.setDescricao((String) req.getParameter("descricao"));
-		patrimonio.setLocalizacao((String) req.getParameter("localizacao"));
-	}
 }
